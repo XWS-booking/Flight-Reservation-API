e92b954f3555f7b84dc21dc403ef0134120f647d
srdjan1210 <businessrdjan@gmail.com>
2023-03-16 19:28:15 +0100

    Added minor util method for responses

--- src/shared/responses.go
+++ src/shared/responses.go
@@ -14,3 +14,12 @@ func BadRequest(resp http.ResponseWriter, payload any) {
 	resp.WriteHeader(http.StatusBadRequest)
 	json.NewEncoder(resp).Encode(payload)
 }
+
+func JsonResponse(resp http.ResponseWriter, statusCode int, payload interface{}) {
+	resp.WriteHeader(statusCode)
+	json.NewEncoder(resp).Encode(payload)
+}
+
+func EmptyResponse(resp http.ResponseWriter, statusCode int) {
+	resp.WriteHeader(statusCode)
+}
